d7680189c49ad857fd55e4b26cd561e8c108ece3
Maciej Gryka <maciej@rainforestapp.com>
2021-01-16 18:14:44 +0100

    make user a leader when joining an empty team

--- lib/secretwords/game_state.ex
+++ lib/secretwords/game_state.ex
@@ -4,12 +4,20 @@ defmodule Secretwords.GameState do
     word_slots: [],
     grid_size: 5,
     teams: %{},
-    leader_blue: "",
-    leader_red: "",
+    leaders: %{}
   ]
 
   def join(game, color, user_id) do
-    updated_members = [user_id | Map.get(game.teams, color, [])]
+    current_members = Map.get(game.teams, color, [])
+
+    game = case current_members do
+      [] ->
+        IO.puts("making " <> user_id <> " a team leader")
+        %{game | leaders: Map.put(game.leaders, color, user_id)}
+      _ -> game
+    end
+
+    updated_members = [user_id | current_members]
     updated_teams = Map.put(game.teams, color, Enum.uniq(updated_members))
     %Secretwords.GameState{game | teams: updated_teams}
   end
