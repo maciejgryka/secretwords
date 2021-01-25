4047b0a2a1874d7d9c53fdacfdc4521517cd4883
Maciej Gryka <maciej@rainforestapp.com>
2021-01-25 18:39:04 +0100

    reformat some code, add @min_players module attrribute

--- lib/secretwords_web/live/game_live.ex
+++ lib/secretwords_web/live/game_live.ex
@@ -3,63 +3,75 @@ defmodule SecretwordsWeb.GameLive do
 
   alias Secretwords.{GameState, Helpers}
 
+  @min_players 1
+
   def mount(%{"id" => game_id}, session, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Secretwords.PubSub, game_id)
+    end
+
     user_id = session["user_id"]
-    game = Helpers.get_or_create_game(game_id)
-    |> GameState.ensure_membership(user_id)
-    |> Helpers.update_game()
+    game =
+      Helpers.get_or_create_game(game_id)
+      |> GameState.ensure_membership(user_id)
+      |> Helpers.update_game()
 
-    if connected?(socket), do: Phoenix.PubSub.subscribe(Secretwords.PubSub, game_id)
+    socket =
+      socket
+      |> assign(min_players: @min_players)
+      |> assign(user_id: user_id)
+      |> assign(game: game)
+      |> assign(derived: derived_state(game, user_id))
 
-    {
-      :ok,
-      assign(
-        socket,
-        game: game,
-        derived: derived_state(game, user_id),
-        user_id: user_id
-      )
-    }
+      {:ok, socket}
   end
 
   def handle_event("switch_teams", _value, socket) do
-    game = Helpers.get_or_create_game(socket.assigns.game.id)
-    |> GameState.switch_teams(socket.assigns.user_id)
-    |> Helpers.update_game()
+    game =
+      Helpers.get_or_create_game(socket.assigns.game.id)
+      |> GameState.switch_teams(socket.assigns.user_id)
+      |> Helpers.update_game()
 
     {:noreply, update_and_assign(socket, game)}
   end
 
   def handle_event("choose_word", %{"word" => word}, socket) do
-    game = Helpers.get_or_create_game(socket.assigns.game.id)
-    |> GameState.choose_word(word)
-    |> Helpers.update_game()
+    game =
+      Helpers.get_or_create_game(socket.assigns.game.id)
+      |> GameState.choose_word(word)
+      |> Helpers.update_game()
 
     {:noreply, update_and_assign(socket, game)}
   end
 
   def handle_event("next_round", _value, socket) do
-    game = Helpers.get_or_create_game(socket.assigns.game.id)
-    |> GameState.next_round
-    |> Helpers.update_game
-    IO.inspect(game.round)
+    game =
+      Helpers.get_or_create_game(socket.assigns.game.id)
+      |> GameState.next_round
+      |> Helpers.update_game
 
     {:noreply, update_and_assign(socket, game)}
   end
 
   def handle_info({:game_updated, game_id}, socket) do
-    game = Helpers.get_or_create_game(game_id)
-    {:noreply, assign(socket, :game, game)}
+    {:noreply, update_and_assign(socket, Helpers.get_or_create_game(game_id))}
   end
 
   defp update_and_assign(socket, game) do
-    assign(socket, game: game, derived: derived_state(game, socket.assigns.user_id))
+    socket
+    |> assign(game: game)
+    |> assign(derived: derived_state(game, socket.assigns.user_id))
   end
 
   defp derived_state(game, user_id) do
     %{
       is_leader: user_id in Map.values(game.leaders),
-      enough_members: (game.teams |> Map.values |> Enum.map(&length/1) |> Enum.min) > 1
+      enough_members: (
+        game.teams
+        |> Map.values
+        |> Enum.map(&length/1)
+        |> Enum.min
+      ) >= @min_players,
     }
   end
 end
